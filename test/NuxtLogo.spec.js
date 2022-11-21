fb1879007737c337f8680033ffd2bcf546a0b30d
brawlstar21 <108696936+brawlstar21@users.noreply.github.com>
2022-11-21 02:05:27 +0200

    Update NuxtLogo.spec.js

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
-  test('is a Vue test', () => {
+  test('is a Vue tests for vue :)', () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
   })
